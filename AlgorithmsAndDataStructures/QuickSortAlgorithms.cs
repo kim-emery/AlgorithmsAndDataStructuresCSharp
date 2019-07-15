26dbfd7f3d3c60ca66ddb0b8dbf6265a6bfb197c
Kim Emery <kimberley.emery@gmail.com>
2019-07-16 00:41:26 +1200

    Add skeleton for Quicksort algorithm file

---
+++ AlgorithmsAndDataStructures/QuickSortAlgorithms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures
+{
+    class QuickSortAlgorithms
+    {
+        internal static void QuickSort(int[] arr, int left, int right)
+        {
+
+        }
+
+        internal static void Partition(int[] arr, int left, int right)
+        {
+
+        }
+
+
+        // may not need this
+        internal static void Swap()
+        {
+
+        }
+
+        
+    }
+}
